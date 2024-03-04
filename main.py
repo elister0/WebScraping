a03c51e04d1d2c6e97638b837875c4bdb50a3773
Emily Lister <emilylister@MacBook-Air.local>
2024-03-04 15:48:45 -0600

    Added and debugged dress image url to the csv file

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
-
+import re
+import os
 # Makes a request to access the url utilizing a user agent to prevent error 403
 req = urllib.request.Request(
     "https://www.jjshouse.com/all/prom-dresses/color/dark-green?sort=new-arrivals",
@@ -20,14 +21,30 @@ soup = BeautifulSoup(f, "html.parser")
 # Finds the names of all the dresses on the page
 dress_name = soup.findAll("h2", attrs={"class":"goods-name"})
 price = soup.findAll("div", attrs={"class":"p_price"})
+image = soup.findAll("a", attrs = {"class":"mt-common-click mt-common-exposure"})
 # Prints the names
+
 dresses = []
 costs = []
+images = []
 for d in dress_name:
      dresses += [(d.text.strip())]
 
 for c in price:
     costs += [c.text.strip()]
+
+# Create a directory to save the images
+for i in image:
+    try:
+        first=str(i).index("data-src=")+10
+        last = str(i).index("orgin=")-2
+        images += [str(i)[first:last]]
+
+    except:
+        print("no")
+# Iterate over the image links and download the images
+
+
 # Creates a new file called Prom Dresses to store the prom dress information
 dress_file = open("Prom_Dresses.csv", "w")
 # creates a writer to put the information in the spreadsheet based on file
@@ -35,6 +52,6 @@ writer = csv.writer(dress_file)
 # Creates a header
 writer.writerow(["Dress"])
 # Goes through each dress name and puts it in the file
-for n, p in zip(dresses, costs):
-    writer.writerow([n, p])
+for n, p, i in zip(dresses, costs, images):
+    writer.writerow([n, p, i])
 dress_file.close()
